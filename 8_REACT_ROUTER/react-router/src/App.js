09f5e48dad936b6fae48433427e8297c72bfe874
Matheus de Farias Cavalcanti Santos <matheusfsantos@ati.intranet>
2024-08-08 15:49:13 -0300

    feat: finished section 8

--- 8_REACT_ROUTER/react-router/src/App.js
+++ 8_REACT_ROUTER/react-router/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,6 +7,8 @@ import Product from './pages/Product';
 import Info from './pages/Info';
 import NotFound from './pages/NotFound';
 import Navbar from './components/Navbar';
+import Search from "./pages/Search";
+import { SearchForm } from "./components/SearchForm";
 
 function App() {
   return (
@@ -17,11 +19,14 @@ function App() {
       
       <BrowserRouter>
         <Navbar />
+        <SearchForm />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/products/:id/info' element={<Info />} />
           <Route path='/products/:id' element={<Product />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/company" element={<Navigate to="/about" />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
